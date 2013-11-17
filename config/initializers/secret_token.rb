c2240bf74ed9fda634049502ad94834691a8786e
Nick Gronow <nick@datekeeper.org>
2013-11-17 00:27:33 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mitt::Application.config.secret_key_base = 'cd92d8de4aaf3529a6d9cb2f494d87b6fb2955400a695a0de35a703de65f2d3a3fa291d3d76e1bd8f25ed798b2c200d77691736d10091dbe1b5219bb91287628'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mitt::Application.config.session_store :cookie_store, key: '_mitt_session'
