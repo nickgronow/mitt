920c12fa98a66ad7629c00a1b072fc24dd5a341a
Nick Gronow <nick@webonlineventures.com>
2013-12-03 12:13:56 -0500

    Removed vendor bundle

--- vendor/bundle/gems/atomic-1.1.14/ext/siteconf20131116-5459-1sjpnjq.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20131116-5459-d9f1jv"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
--- vendor/bundle/gems/json-1.8.1/ext/json/ext/generator/siteconf20131116-5459-wjka2w.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20131116-5459-tg4x19"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
