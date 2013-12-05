08e53e25f4bb1b3bf62c4648f098c993e1613398
Nick Gronow <nick@webonlineventures.com>
2013-12-05 11:32:30 -0500

    some final updates

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publisher_key => ENV['STRIPE_TEST_PUBLISHER_KEY'],
-  :secret_key      => ENV['STRIPE_TEST_SECRET_KEY']
+  :publisher_key => ENV['STRIPE_LIVE_PUBLISHER_KEY'],
+  :secret_key      => ENV['STRIPE_LIVE_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
