9c9212137dc36c0580ed2393935a2f5b7f41484a
Nick Gronow <nick@webonlineventures.com>
2013-12-05 19:48:23 -0500

    Enabling stripe charges

--- app/controllers/donate_controller.rb
+++ app/controllers/donate_controller.rb
@@ -6,7 +6,7 @@ class DonateController < ApplicationController
 	end
 
 	def thanks
-		# Save this contact to the database
+		# Get the data
 		@donation = Donation.new
 		@donation.name = params["full-name"]
 		@donation.phone = params[:phone]
@@ -14,6 +14,26 @@ class DonateController < ApplicationController
 		@donation.support_type = params["donate-amount"] ? 'sponsor' : 'auction'
 		@donation.amount = params["donate-amount"] ? params["donate-amount"].gsub(/[^0-9.]/, '') : 0
 		@donation.title = params[:title]
+		
+		# Create the charge to the supporter
+		customer = Stripe::Customer.create(
+			:email => 'example@stripe.com',
+			:card  => params[:token]
+		)
+
+		charge = Stripe::Charge.create(
+			:customer    => customer.id,
+			:amount      => round(@donation.amount * 100),
+			:description => 'Sponsor Donation for Heart in the Tropics',
+			:currency    => 'usd'
+		)
+
+		rescue Stripe::CardError => e
+			flash[:error] = e.message
+			redirect_to "donate/landing"
+		end
+
+		# Save this contact to the database
 		@donation.save
 
 		# Send an email to contact
